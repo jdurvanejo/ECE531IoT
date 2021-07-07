19dd39f17d4c972ea3a8962505e2f74462b7372c
jason <jdurvanejo@gmail.com>
2021-07-06 20:23:36 -0600

    got the time part woking

---
+++ wk3_assignment/time_daemon.c
@@ -0,0 +1,95 @@
+//used arm-linux-gcc to compile
+
+#define _POSIX_SOURCE
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <stdio.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+#undef _POSIX_SOURCE
+
+#define ERR_WTF	   5
+#define ERR_CHDIR  4
+#define ERR_SETSID 3
+#define ERR_FORK   2
+#define OK	   1
+#define DAEMON_NAME "sampled"
+#define ERROR_FORMAT "%s"
+
+static void _signal_handler(const int signal) {
+  switch(signal) {
+    case SIGHUP:
+      break;
+    case SIGTERM:
+      syslog(LOG_INFO, "received SIGTERM, exiting.");
+      closelog();
+      exit(OK);
+      break;
+    default:
+      syslog(LOG_INFO, "received unhandled signal");
+  }
+}
+
+static void _do_work(void) {
+  time_t time_current;
+  struct tm *time_stuff;
+
+  while(true)
+  {
+    time(&time_current);
+    time_stuff = localtime(&time_current);
+    syslog(LOG_INFO, "current time: %i:%i:%i\n",time_stuff->tm_hour,time_stuff->tm_min,time_stuff->tm_sec);
+ // for (int i = 0; true; i++) {
+ //   syslog(LOG_INFO, "iteration: %d", i);
+    sleep(1);
+  }
+}
+
+int main(void) {
+
+   openlog(DAEMON_NAME, LOG_PID | LOG_NDELAY | LOG_NOWAIT, LOG_DAEMON);
+  syslog(LOG_INFO, "starting sampled");
+
+
+  pid_t pid = fork();
+  if (pid < 0) {
+    syslog(LOG_ERR, ERROR_FORMAT, strerror(errno));
+    return ERR_FORK;
+  }
+
+  if (pid > 0) {
+    return OK;
+  }
+
+  if (setsid() < -1) {
+    syslog(LOG_ERR, ERROR_FORMAT, strerror(errno));
+    return ERR_SETSID;
+  }
+
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  umask(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+  if (chdir("/") < 0) {
+    syslog(LOG_ERR, ERROR_FORMAT, strerror(errno));
+    return ERR_CHDIR;
+  }
+
+  signal(SIGTERM, _signal_handler);
+  signal(SIGHUP,_signal_handler);
+
+
+  _do_work();
+
+  return ERR_WTF;
+
+}
